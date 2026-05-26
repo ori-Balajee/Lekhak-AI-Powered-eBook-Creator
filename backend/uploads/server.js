47e11c0897f6c62c04839e36815d5573354df802
ori-Balajee <balajee4drip@gmail.com>
2026-05-27 04:42:17 +0530

    MongoDB connection established

--- backend/uploads/server.js
+++ backend/uploads/server.js
@@ -1,15 +1,29 @@
+// Loads variables from a .env file into process.env
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const connectDB = require("./config/db");
 
+const express = require("express");
+
+// Enables cross-origin requests. 
+// Without it : frontend (React) cannot talk to backend
+const cors = require("cors");
+
+// Help work with files and foldershttps://www.youtube.com/watch?v=gmuTjeQUbTM&list=PLNKUcsB3XI_68EIWp7nLQIj4joCrlsWeN&index=197
+const path = require("path");
+
+// Imports CUSTOM function from folder config to connect MongoDB
+const connectDB = require("../config/db");
+
+// backend application container
 const app = express();
 
+// MORE
 app.use(
     cors({
+        // allows ANY frontend (not secure for production)
         origin: "*",
+        // allows HTTP methods
         methods: ["GET", "POST", "PUT", "DELETE"],
+        // allows extra info in requests
         allowedHeaders: ["Content-Type","Authorization"],
     })
 )
@@ -17,8 +31,10 @@ app.use(
 // connect Database
 connectDB();
 
+// lets server read JSON from requests
 app.use(express.json());
 
+// MORE
 app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
 
 // server start
